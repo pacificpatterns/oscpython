a03e6e32c6569345bd4f03e0b99f50566f69372c
Christopher Petersen <57047610+pacificpatterns@users.noreply.github.com>
2022-11-16 15:47:26 -0800

    made folder for unused documents

--- test_clientsendmsgtosever.py
+++
@@ -1,9 +0,0 @@
-from pythonosc.udp_client import SimpleUDPClient
-
-ip = "127.0.0.1"
-port = 1337
-
-client = SimpleUDPClient(ip, port)  # Create client
-
-client.send_message("/some/address", 123)   # Send float message
-client.send_message("/some/address", [1, 2., "hello"])  # Send message with int,
